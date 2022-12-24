73c6b9267b1273f270e731ded4a7c3c743a90fc7
AntonioCopete <antcopjim@gmail.com>
2022-12-24 17:15:51 +0100

    fix minting

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xB037533Be6A8bB842AFb1247ed7763F49aca17fE";
+const WHITELIST_CONTRACT_ADDRESS = "0xfEbD07148b86616138dA27912d9aa6697BD67929";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-one-green.vercel.app/api/";
 
